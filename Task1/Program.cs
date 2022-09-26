2b0d1572074df3722f01e8243a59fdb2d15e3f0b
Lika Sizih <likasizih@mail.ru>
2022-09-26 10:37:06 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿void Task1()
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    int a = int.Parse(Console.ReadLine());
+    int result = ((a / 10) % 10);
+    Console.WriteLine(result);
+}
+
+Task1();
