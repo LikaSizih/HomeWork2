2280eeff22ee8b3aad1bd6ea2e9d98a38fe97a59
Lika Sizih <likasizih@mail.ru>
2022-09-26 10:37:29 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿void Task3()
+{
+    Console.WriteLine("Введите цифру, обозначающую день недели: от 1 до 7: ");
+    int a = int.Parse(Console.ReadLine());
+    if (a < 6)
+        {
+            Console.WriteLine("Это будний день");
+        }
+    else
+    {
+        Console.WriteLine("Выходной день ");
+    }
+}
+Task3();
