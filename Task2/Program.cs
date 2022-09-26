501b80e4a9b48d74bc161b4a50bc1c15b978d2e2
Lika Sizih <likasizih@mail.ru>
2022-09-26 10:37:19 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿void Task2()
+{
+    Console.WriteLine("Введите число: ");
+    int a = int.Parse(Console.ReadLine());
+    if (a < 99)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    else
+        {
+            while (a > 999)
+            {
+                a/=10;
+            }
+            Console.WriteLine(a%10);
+        }
+}
+Task2();
